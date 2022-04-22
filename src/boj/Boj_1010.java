ac6d142f7111533454e2a60503cd4a0a39786c91
endrmfek <alstn9469@naver.com>
2022-04-22 22:31:09 +0900

    2022-04-22 boj

---
+++ src/boj/Boj_1010.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Boj_1010 {
+	public static void main(String[] args) {
+		long[][] dp = new long[31][31];
+		for(int i=0; i<=30; i++) {
+			dp[i][1] = i;
+			dp[i][i] = 1;
+			dp[i][0] = 1;
+		}
+		
+		for(int i=2; i<=30; i++) {
+			for(int j=1; j<i; j++) {
+				dp[i][j] = dp[i-1][j] + dp[i-1][j-1]; 
+			}
+		}
+		
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		for(int i=0; i<tc; i++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			System.out.println(dp[m][n]);
+		}
+	}
+}
