7c8f2fca9a53061ae532bd986be0c2583946799f
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2022-06-16 13:36:15 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -13,37 +13,37 @@ data.sort()
 
 for k in range(n):
   target = data[k]
-  temp = data[:k] + data[k+1:n+1]
+  # temp = data[:k] + data[k+1:n+1]
   i=0
-  #j=n-1
-  j=len(temp)-1
+  j=n-1
+  # j=len(temp)-1
   #풀이1
-  # while(i < j):
+  while(i < j):
     
-  #   if target == data[i] + data[j]:
-  #     #본인은 포함되어있으면 안됩니다. ->조건문으로 제외
-  #     if (i != k and j != k): 
-  #       count+=1
-  #       break;
-  #     elif i==k:
-  #       i+=1
-  #     else:
-  #       j-=1
-  #   elif data[i] + data[j] < target:
-  #     i+=1
-  #   else:
-  #     j-=1
+    if target == data[i] + data[j]:
+      #본인은 포함되어있으면 안됩니다. ->조건문으로 제외
+      if (i != k and j != k): 
+        count+=1
+        break;
+      elif i==k:
+        i+=1
+      else:
+        j-=1
+    elif data[i] + data[j] < target:
+      i+=1
+    else:
+      j-=1
 
   #풀이2 아싸리 본인을 제외한 리스트를 생성
-  while (i<j):
-    if target == temp[i]+ temp[j]:
-      count+=1
-      break;
-    else:
-      if target > temp[i] + temp[j]:
-        i += 1
-      else:
-        j -= 1
+  # while (i<j):
+  #   if target == temp[i]+ temp[j]:
+  #     count+=1
+  #     break;
+  #   else:
+  #     if target > temp[i] + temp[j]:
+  #       i += 1
+  #     else:
+  #       j -= 1
   
 print(count)
     
