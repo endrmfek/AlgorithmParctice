bc5c51243698d2c16b5294745dd8d42bf914f63f
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2022-07-13 18:33:45 +0900

    [level 3] Title: 가장 먼 노드, Time: 29.00 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+class Solution {
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        int[] distance = new int[n+1];
+        
+        Arrays.fill(distance, -1);
+        
+        for (int i=0; i<=n; i++) {
+        	graph.add(new ArrayList<>());
+        }
+        
+        for (int i=0; i<edge.length; i++) {
+        	int start = edge[i][0];
+        	int end = edge[i][1];
+        	
+        	graph.get(start).add(end);
+        	graph.get(end).add(start);
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        distance[1] = 0;
+        
+        while (!q.isEmpty()) {
+        	int startIndex = q.poll();
+        	
+        	for(int nextIndex : graph.get(startIndex)) {
+        		if (distance[nextIndex] == -1) {
+        			q.add(nextIndex);
+        			distance[nextIndex] = distance[startIndex] + 1;
+        		}
+        	}
+        }
+        
+        int max = 0;
+        for (int i : distance) {
+        	if(i > max) {
+        		max = i;
+        	}
+        }
+        for (int i : distance) {
+        	if (i == max) {
+        		answer += 1;
+        	}
+        }
+        
+        return answer;
+    }
+}
