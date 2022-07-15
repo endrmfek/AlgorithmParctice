385fa26791321bb064fab01333bf5facb4580a22
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2022-07-15 15:29:43 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static int solution(int left, int right) {
+        int answer = 0;
+        for(int i=left; i<=right; i++) {
+        	int check = check(i);
+        	System.out.println(check);
+        	answer += check;
+        }
+        return answer;
+    }
+	
+	public static int check(int num) {
+		int pow = (int) (num/2)+1;
+		int count = 1;
+		for(int i=1; i<pow; i++) {
+			if ((num % i) == 0) {
+				count++;
+			}
+		}
+		if (count%2 == 0) {
+			return num;
+		}
+		else {
+			return -num;
+		}
+	}
+}
