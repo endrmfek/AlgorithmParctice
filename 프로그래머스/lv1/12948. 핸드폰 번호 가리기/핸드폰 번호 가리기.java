e72b07056d374809206ed9219929f211f01ba954
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2022-08-08 17:04:09 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.75 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String phone_number) { // 11
+        String answer = "";
+        int n = phone_number.length();
+        String prefix = phone_number.substring(0,n-4); // 0~6
+        prefix = prefix.replaceAll("[0-9]", "*");
+        String postfix = phone_number.substring(n-4); //
+        return prefix +postfix;
+    }
+}
