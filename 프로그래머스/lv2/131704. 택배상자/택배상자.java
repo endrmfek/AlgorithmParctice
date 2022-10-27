508d507f4816438ea95fb6105b59d246f64e62c8
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2022-10-27 15:22:06 +0900

    [level 2] Title: 택배상자, Time: 107.26 ms, Memory: 152 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -0,0 +1,58 @@
+import java.util.*;
+class Solution {
+    //한 방향으로만.
+    //1~n번.
+    //미리 알려준 순서대로
+    //보조 컨테이너는 맨 앞의 상자만 -> 선입후출? 스택
+    //4 3 1 2 5 ->
+    //더이상 실을 수 없을때 종료.
+    //영재가 몇개의 상자를 실을 수 있는지 return
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> assist = new Stack<>();
+        ArrayList<Integer> container = new ArrayList<>();
+        int[] arr = new int[order.length];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = i+1;
+        }
+        int arrIndex = 0;
+        int orderIndex = 0;
+//        [4,3,1,2,5] 1 2 3 stack 4매칭 break; 3 -> 스택뒤져 ㅇㅋ
+        //[1,2,3,4,5] 5
+        //[5,4,3,2,1]
+        //[1,2,3,4,5]
+        while(container.size() != order.length && arrIndex != arr.length) {
+            if(!assist.isEmpty() && assist.peek() == order[orderIndex]) {
+                int number = assist.pop();
+                container.add(number);
+                orderIndex++;
+                continue;
+            }
+            // System.out.println(arrIndex + " " + orderIndex);
+            if(arr[arrIndex] == order[orderIndex]) {
+                container.add(arr[arrIndex]);
+                arrIndex++;
+                orderIndex++;
+
+            } else {
+                assist.add(arr[arrIndex]);
+                arrIndex++;
+            }
+        }
+
+        while(true) {
+            if(!assist.isEmpty() && assist.peek() == order[orderIndex]) {
+                int number = assist.pop();
+                container.add(number);
+                orderIndex++;
+
+            } else {
+                break;
+            }
+        }
+
+        // System.out.println(container.toString());
+        return container.size();
+    }
+}
