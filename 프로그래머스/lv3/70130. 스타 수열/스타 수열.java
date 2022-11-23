80e423a65fd2d3fe5bda27253c399174147c4e9e
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2022-11-23 15:08:02 +0900

    [level 3] Title: 스타 수열, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/70130. 스타 수열/스타 수열.java
@@ -0,0 +1,32 @@
+class Solution {
+    //2이상의 짝수
+    //교집합의 원소가 1개.
+    //x[n-1] != x[n]
+
+    //스타수열의 길이.
+    public int solution(int[] a) {
+        int answer = -1;
+        int n = a.length;
+        if(n == 1) return 0;
+        int[] numberList = new int[n];
+
+        for(int number : a) {
+            numberList[number]++;
+        }
+        
+        for(int i=0; i<numberList.length; i++) {
+            if(numberList[i] == 0) continue;
+            if(numberList[i] < answer) continue;
+            int temp = 0;
+            for(int j=0; j<n-1; j++) {
+                if(!(a[j] == i || a[j+1] == i)) continue;
+                if(a[j] == a[j+1]) continue;
+                
+                temp++;
+                j++;
+            }
+            answer = Math.max(answer, temp);
+        }
+        return answer * 2;
+    }
+}
