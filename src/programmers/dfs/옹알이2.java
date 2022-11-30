35aa815bdb2a7cb831eea6959eeb8651f14790b9
endrmfek <alstn9469@naver.com>
2022-11-30 23:23:48 +0900

    i hope i can understand it

--- src/programmers/dfs/옹알이2.java
+++ src/programmers/dfs/옹알이2.java
@@ -3,63 +3,6 @@ package programmers.dfs;
 import java.util.*;
 
 public class 옹알이2 {
-    boolean[] visited = new boolean[4];
-    Set<String> set;
-    public int solution2(String[] babbling) {
-        int answer = 0;
-        String[] babble = {"aya", "ye", "woo", "ma"}; //이정도 할수있걷느여
-
-        set = new TreeSet<>(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if(o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                }
-                return o1.length() - o2.length();
-            }
-        });
-
-
-        for(int i=1; i<=4; i++) {
-            //단어를 1개부터 4개까지쓰는 부분순열? 을 만들어야됨.
-            dfs(0, i , babble, new ArrayList<>());
-        }
-
-        for(String b : babbling) {
-            //이 순열안에 포함되어있니?
-            if(set.contains(b)) {
-                answer++;
-            }
-        }
-        return answer;
-    }
-    void dfs(int depth, int n , String[] arr, ArrayList<String> words) {
-        if(depth == n) {
-            StringBuilder sb = new StringBuilder();
-            if(words.size() == 1) {
-                for(String word : words) {
-                    sb.append(word);
-                }
-            }
-            else {
-                for(int i=0; i<words.size()-1; i++) {
-                    sb.append(words.get(i));
-                    if(words.get(i).equals(words.get(i+1))) { // 걍 중복되면 쳐넣지마
-                        return;
-                    }
-                }
-                sb.append(words.get(words.size()-1));
-            }
-            set.add(sb.toString());
-            return;
-        }
-
-        for(int i=0; i<4; i++) { //양쪽중복을 제거하려면?
-                words.add(arr[i]);
-                dfs(depth+1 , n, arr, words);
-                words.remove(words.size()-1);
-        }
-    }
 
     public int solution(String[] babbling) {
         int answer = 0;
@@ -100,6 +43,6 @@ public class 옹알이2 {
 
     public static void main(String[] args) {
         옹알이2 a = new 옹알이2();
-        a.solution2(new String[] {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"});
+        a.solution(new String[] {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"});
     }
 }
---
+++ src/programmers/implement/과일장수.java
@@ -0,0 +1,28 @@
+package programmers.implement;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+public class 과일장수 {
+    public int solution(int k, int m, int[] score) {
+        //1 ~ k 점.
+        //한 상자에 사과를 m개씩.
+        //가장 낮은 점수가 p일때 -> 가격이 p*m.
+        //k = 3 , m = 4 사과 7개 점수가
+        //최저 사과 점수 x 한 상자에 담긴 사과 개수 x 상자의 개수
+        int answer = 0;
+        int box = score.length / m;
+        int[][] apple = new int[box][m];
+        score = Arrays.stream(score).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        for(int i=0; i<box; i++) {
+            for(int j=0; j<m; j++) {
+                apple[i][j] = score[(i*m)+j];
+            }
+            answer += (apple[i][m-1] * m);
+        }
+
+
+        return answer;
+    }
+}
