c49e6ef117ec2e96d3782901b95297b7486101dc
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-01-09 13:00:02 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.80 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Solution {
+    //1. 서비스가입자
+    //2. 판매액
+    //dfs잖아
+    //1<=user=n<=100
+    //1<=emoticons = m<=7
+    static int emoPlus = 0;
+    static int totalPrice = 0;
+    static List<int[]> result = new ArrayList<>();
+    public static int[] solution(int[][] users, int[] emoticons) {
+        int[] answer = new int[users.length];
+        int[] arr = new int[] {10,20,30,40};
+        int[] discount = new int[emoticons.length];
+
+        dfs(0, arr,discount, users, emoticons);
+
+        result.sort(((o1, o2) -> {
+            if(o2[0] == o1[0]) {
+                return o2[1] - o1[1];
+            }
+            return o2[0]-o1[0];
+        }));
+//        System.out.println(result.get(0)[0] + " " +  result.get(0)[1]);
+        return new int[] {result.get(0)[0], result.get(0)[1]};
+    }
+
+    private static void dfs(int depth, int[] arr, int[] discount, int[][] users, int[] emoticons) {
+        if(depth == discount.length) {
+            //할인율 완전탐색
+            int emo = 0;
+            int price = 0;
+//            System.out.println(Arrays.toString(discount));
+            for(int[] user : users) {
+                int myDiscount = user[0];
+                int myPrice = user[1];
+                int buy = 0;
+
+                for(int i=0; i<emoticons.length; i++) { // 사람당 이모티콘
+                    if (discount[i]>= myDiscount) { // 할인율 높으면 사야지
+                        buy += (double)(emoticons[i] /100) * (100- discount[i]);
+//                        System.out.print("discount = " + discount[i] + " buy = " + buy + " ");
+                    }
+                }
+
+                if(buy >= myPrice) {
+                    emo++;
+                } else {
+                    price += buy;
+                }
+            }
+            result.add(new int[]{emo, price});
+            return;
+        }
+        //10 10 10 10
+        //10 10 10
+        //10 10 20 10
+        for(int i=0; i<arr.length; i++) {
+            discount[depth] = arr[i];
+            dfs(depth+1, arr, discount, users, emoticons);
+        }
+    }
+}
