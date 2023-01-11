d0971e988fe5e19b643f15caae80d3fa77bbc46f
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-01-11 16:10:53 +0900

    [level 2] Title: [1차] 캐시, Time: 2.44 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    // 0<=캐시크기<=30
+    // LRU? 가장 오랫동안 참조되지 않은 페이지 교체
+    //hit =1 , miss = 5
+    public static int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        Queue<String> cache = new LinkedList<>(); // 순서를 보장하자. (index)
+
+        if(cacheSize == 0) {
+            return 5*cities.length;
+        }
+
+        for(String c : cities) { // LRU -> 가장 사용안한거 빼버리는거잖아
+            String city = c.toLowerCase();
+            if(cache.contains(city)) { // cache에 들어있으면
+                cache.remove(city);
+                cache.add(city);
+                answer++;
+            } else { //캐시에 없을때
+                if(cache.size() == cacheSize) { // 꽉찼는데 없어?
+                    cache.poll(); // 맨위에꺼 버리고
+                }
+                cache.add(city);
+                answer+=5;
+            }
+            
+        }
+        return answer;
+    }
+}
