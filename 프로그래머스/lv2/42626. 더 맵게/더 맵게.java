bd331b68d5b7c71992d606d793da7fbca49d0327
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-02-09 20:04:53 +0900

    [level 2] Title: 더 맵게, Time: 1567.14 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    //섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        for(int s : scoville) {
+            q.add(s);
+        }
+        
+        while(q.peek() < K) {
+            int first = q.poll();
+            int second = q.poll();
+
+            int newScoville = first + (second * 2);
+            q.add(newScoville);
+            answer++;
+            
+            if(q.size() == 1 && q.peek() < K) {
+                return -1;
+            }
+        }
+        
+        return answer;
+    }
+}
