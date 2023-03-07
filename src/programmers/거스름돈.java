f87478d32f1224a564eb9a6815621994cbad7f8e
endrmfek <alstn9469@naver.com>
2023-03-07 21:34:44 +0900

    i hope i understand it

--- src/programmers/거스름돈.java
+++ src/programmers/거스름돈.java
@@ -1,23 +1,20 @@
 package programmers;
 
 public class 거스름돈 {
-    //방법의 수??..
-    //1,000,000,007 을 나눠라.
-    //dp잖아 이거.
+    //최소 방법 이런게 아니라
+    //n원을 거슬러 줄 수 있는 방법의 수 -> 전체 다 구하면돼.
     public int solution(int n, int[] money) {
         int answer = 0;
-        //1,2,5 <- 하나도 겹치는게 없어..
         int[] dp = new int[n+1];
-        dp[0] = 1; // 0원을 뭐어떻게 할건디?..
+        dp[0] = 1; //왜 1일까?
 
         for(int m : money) {
+            //각 돈마다 만들 수 있는 경우의 수 모두 구해
             for(int i=m; i<=n; i++) {
-                //dp는 항상 그 전 답을 알고있다고 가정하고 풀어야됨.
                 dp[i] += dp[i-m];
                 dp[i] = dp[i] % 1000000007;
             }
         }
-
         return dp[n];
     }
 }
