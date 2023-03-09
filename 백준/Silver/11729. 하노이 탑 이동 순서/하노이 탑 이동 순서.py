18a1246378e50510dcb67770f66c7f9f09502cf3
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-03-09 20:21:12 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 728 ms, Memory: 216668 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,18 @@
+n = int(input())
+move = []
+
+def hanoi(n, a , b, c):
+  if n == 1:
+    move.append((a,c))
+  
+  else:
+    hanoi(n-1, a,c,b)
+    move.append((a,c))
+    hanoi(n-1, b,a,c)
+
+  
+hanoi(n, 1,2,3)
+print(len(move))
+for i in move:
+  a,b = i
+  print(f'{a} {b}')
