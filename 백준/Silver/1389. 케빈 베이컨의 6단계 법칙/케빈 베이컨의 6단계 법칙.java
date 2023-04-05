e809296f6b6a8bd3b00c5bff6f65daa3ac0b979a
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-04-05 20:10:20 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,64 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n,m;
+	static int distance[][];
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		distance = new int[n+1][n+1];
+		
+		for(int i=1; i<n+1; i++) {
+			for(int j=1; j<n+1; j++) {
+				if(i==j) distance[i][j] = 0;
+				else distance[i][j] = 10000001;
+			}
+		}
+		
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			distance[start][end] = 1;
+			distance[end][start] = 1; //양방향.
+		}
+		
+		//플로이드 워셜 시작
+		for(int k=1; k<n+1; k++) {
+			for(int i=1; i<n+1; i++) {
+				for(int j=1; j<n+1; j++) {
+					if(distance[i][j] > distance[i][k] + distance[k][j]) {
+						distance[i][j] = distance[i][k] + distance[k][j];
+					}
+				}
+			}
+		}
+		
+		int min = Integer.MAX_VALUE;
+		int result = -1;
+		
+		for(int i=1; i<=n; i++) {
+			
+			int temp = 0;
+			for(int j=1; j<=n; j++) {
+				temp = temp + distance[i][j];	//각 행의 합
+			}
+			
+			if(min > temp) { //행의 값이 가장 작은거 골라서
+				min = temp;
+				result = i;	//그거 인덱스 저장
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+}
