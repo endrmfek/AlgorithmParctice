654e53fede180a0607cf4eb57fc8a6fbd635fb6e
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-04-07 20:10:20 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 208 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+//쓸때 없는 변수를 너무 많이 사용함.
+//나동빈님께서는 한가지 변수를 사용해 반은 더하고 반은 빼서 0이되면 조건을 만족하는 방법으로 해결.
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String data = sc.nextLine();
+		int len = data.length()/2;
+		int a = 0;
+		int b = 0;
+		
+		for(int i=0; i<len; i++) {
+			int temp = data.charAt(i) - '0';
+			a += temp;
+		}
+		
+		for(int j=len; j<data.length(); j++) {
+			int temp = data.charAt(j) -'0';
+			b += temp;
+		}
+		
+		if( a==b) {
+			System.out.println("LUCKY");
+		} else {
+			System.out.println("READY");
+		}
+	}
+}
