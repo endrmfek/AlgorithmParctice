a164194a02f35f10cb32e5652df281ac4965a6c6
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-04-15 12:10:10 +0900

    [Silver III] Title: 문자열 집합, Time: 1088 ms, Memory: 69468 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,29 @@
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int answer = 0;
+		Set<String> dict = new HashSet<>();
+		
+		for(int i=0; i<n; i++) {
+			dict.add(sc.next());
+		}
+		
+		for(int i=0; i<m; i++) {
+			String data = sc.next();
+			if (dict.contains(data)) {
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}	
+
+
