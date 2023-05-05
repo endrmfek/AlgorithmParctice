6218b4779f993834d4f8833c61a78c8e2304ee62
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-05-05 10:23:40 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		Integer[] a = new Integer[n];
+		
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(a);
+		int max = a[n-1];
+		float result = 0;
+		for(int i=0; i<n; i++) {
+			result += a[i];
+		}
+		
+		System.out.println( result * 100 / max / n);
+	}
+}
