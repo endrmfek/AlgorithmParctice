b030b84bdcfda1dae83c343ba211bf71ed3ff6b3
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-05-21 22:15:01 +0900

    [unrated] Title: 덧칠하기, Time: 0.84 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        
+        int next = 0;
+        for(int i=0; i<section.length; i++) {
+            //칠하고
+            answer++;
+            //옮기고
+            next = section[i] + m;
+            
+            while(i+1 < section.length && next > section[i+1]) {
+                i++;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
