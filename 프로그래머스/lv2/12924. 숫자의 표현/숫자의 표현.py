42c172e0a24e88285ddb97fe4da696fbeae86f06
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-06-22 20:12:54 +0900

    [level 2] Title: 숫자의 표현, Time: 5.47 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,3 +1,16 @@
 def solution(n):
     answer = 0
+    
+    for i in range(1,n+1):
+        hap = 0
+        for j in range(i,n+1):
+            hap += j
+            
+            if hap == n:
+                answer+=1
+                break;
+            
+            if hap > n:
+                break;
+                
     return answer
