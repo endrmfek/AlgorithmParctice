e5e898075d5c2faef4081a360897a8ee0ae54831
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-06-25 17:03:35 +0900

    [level 2] Title: N-Queen, Time: 971.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.py
@@ -28,24 +28,33 @@
 #     queen(n,0,row)
 #     return answer
 
-answer = 0
+ans = 0
+num = 0
+chkX = [False for i in range(32)]
+chkCross1 = [False for i in range(32)]
+chkCross2 = [False for i in range(32)]
+
+def nq(y,n):
+    global ans
+    x = 0
+    if y > n:
+        ans+=1
+        return
+    
+    for x in range(1, n+1):
+        if chkX[x] or chkCross1[y+x] or chkCross2[(y-x) + n]:
+            continue
+        chkX[x] = True
+        chkCross1[y+x] = True
+        chkCross2[(y-x) + n] = True
+        
+        nq(y+1, n)
+        
+        chkX[x] = False
+        chkCross1[y+x] = False
+        chkCross2[(y-x) + n] = False
+
 def solution(n):
-    global answer
-    row = [True]*n
-    r_dig = [True]*2*n
-    l_dig = [True]*2*n
-    def queen(r, remain):
-        global answer
-        if remain:
-            for idx,i in enumerate(remain):
-                if row[i] and r_dig[r-i] and l_dig[r+i]:
-                    next_remain = remain[:idx] + remain[idx+1:]
-                    row[i] = r_dig[r-i] = l_dig[r+i] = False
-                    queen(r+1, next_remain)
-                    row[i] = r_dig[r-i] = l_dig[r+i] = True
-        else:
-            answer += 1
+    nq(1,n)
+    return ans
 
-    queen(0, list(range(n)))
-
-    return answer
