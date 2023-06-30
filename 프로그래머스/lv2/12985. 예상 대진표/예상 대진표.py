53ae2e048e5b9e69cd87efc3f470cca596ff56dd
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-06-30 21:12:12 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,9 +1,21 @@
-import math
 def solution(n,a,b):
     answer = 0
+    #a가 작은수 b가 큰수
+    if a > b:
+        a,b = b,a
 
     while a != b:
+        #짝수면 ex) 4
+        if a % 2 == 0:
+            a = a//2
+        else:
+            a = (a+1)//2
+            
+        if b % 2 == 0:
+            b = b//2
+        else:
+            b = (b+1)//2
         answer += 1
-        a, b = (a+1)//2 , (b+1)//2
+        
 
     return answer
