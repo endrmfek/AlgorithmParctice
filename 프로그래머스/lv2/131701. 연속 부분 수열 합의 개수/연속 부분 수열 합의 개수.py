6bd70796b494fbce2e632f4f457d15f7feccf9e8
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-07-11 20:23:34 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 329.28 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,4 +1,23 @@
 def solution(elements):
+    n = len(elements)
+    result = set()
+    
+    #각 숫자에서
+    for i in range(n):
+        _sum = elements[i]
+        result.add(_sum)
+        #n까지 더할때마다 set에 넣어. 
+        for size in range(i+1, i+n):
+            _sum += elements[size%n]
+            result.add(_sum)
+    
+    return len(result)
+
+            
+
+
+# O(n^3)
+def solution1(elements):
     answer = 0
     arr = elements * 2
     result = set()
