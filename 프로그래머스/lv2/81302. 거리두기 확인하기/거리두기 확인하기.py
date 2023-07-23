fda7f3164b55500b0c36aded94dd144c522bf2b7
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-07-23 21:00:00 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,6 +1,6 @@
 from collections import deque
-
-def solution(places):
+from itertools import combinations
+def solution1(places):
     #파티션을 신경쓰지말고 BFS로 완전 탐색 갈기자.
     answer = []
     
@@ -47,6 +47,51 @@ def check(data : list):
                     if data[nr][nc] == "P" and distance[row][col] <= 1:
                         return 0
     return 1                
-                
+
+#=============================================================================
+def solution(places):
+    answer = []
+    
+    for place in places:
+        # 각 반에 대하여
+        person = []
+        for i, row in enumerate(place):
+            for j, col in enumerate(row):
+                if place[i][j] == 'P':
+                    person.append((i,j))
+                    
+        check = True
+        for p1, p2 in combinations(person, 2):
+            p1Row, p1Col, p2Row, p2Col = p1[0], p1[1], p2[0], p2[1]
+            if abs(p1Row - p2Row) + abs(p1Col - p2Col) <= 1:
+                check = False;
+                break;
             
+            #같은 X축에 or 같은 Y축에 or 대각선에
+            if abs(p1Row - p2Row) + abs(p1Col - p2Col) == 2:
+                #같은 Row축에 있을때
+                if p1Row == p2Row:
+                    if place[p1Row][(p1Col+p2Col)//2] == 'O':
+                        check= False;
+                        break;
+                #같은 Col축에 있을때
+                elif p1Col == p2Col:
+                    if place[(p1Row+p2Row)//2][p1Col] == 'O':
+                        check= False;
+                        break;
+                #대각선에 있을때
+                else:
+                    if place[p1Row][p2Col] == 'O' or place[p2Row][p1Col] == 'O':
+                        check= False;
+                        break;
+        if check:
+            answer.append(1)
+        else:
+            answer.append(0)
+            
+    return answer
+                    
+            
+    
+
     
