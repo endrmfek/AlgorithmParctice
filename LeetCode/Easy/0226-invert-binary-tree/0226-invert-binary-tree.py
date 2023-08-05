95f6c38bf4c2e63f5e5d90e13947b1b35b0b6980
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-08-05 21:00:00 +0900

    [Easy] Title: , Time: 13 ms (75.44%), Memory: 13.3 MB (67.47%) - LeetHub

--- LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.py
+++ LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -4,13 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution(object):
+    
     def invertTree(self, root):
-        # print(root)
-        
-        if not root:
-            return None
-        
-        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
-        
+        # solution1
+        # if not root:
+        #     return None
+        # root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        # return root
+    
+        # solution2
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            
+            if node:
+                node.left , node.right = node.right, node.left
+                q.append(node.left)
+                q.append(node.right)
+                
         return root
