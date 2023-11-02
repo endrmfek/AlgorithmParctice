c7ccf4d5c8497ce8d111b460a453a134f2543ad9
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-11-02 18:02:40 +0900

    [Silver IV] Title: 수 찾기, Time: 220 ms, Memory: 144144 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,10 @@
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end)// 2
-
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else :
-            start = mid + 1
-    return None
-
 n = int(input())
-data = list(map(int,input().split()))
-data.sort()
-
+arr = set(map(int, input().split()))
 m = int(input())
-targets = list(map(int,input().split()))
+data = list(map(int,input().split()))
 
-for target in targets:
-  result = binary_search(data, target, 0, n-1)
-  
-  if result is not None:
-    print(1)
-  else:
-    print(0)
-
+for num in data:
+    if num in arr:
+        print(1)
+    else:
+        print(0)
