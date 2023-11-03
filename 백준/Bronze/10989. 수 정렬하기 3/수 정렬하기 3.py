454bc979da7a762789eea0027135223d55185a23
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-11-03 21:56:21 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4452 ms, Memory: 128052 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,15 @@
+import heapq
+
+n = int(input())
+arr = [0] * 10001 
+
+for i in range(n):
+    num = int(input())
+    arr[num] += 1
+
+for i in range(len(arr)):
+    if arr[i] != 0:
+        for _ in range(arr[i]):
+            print(i)
+    
+    
