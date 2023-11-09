f0853c894bc70a905c154fcaacd86485e0ad75af
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-11-09 20:08:40 +0900

    [Silver II] Title: 스택 수열, Time: 180 ms, Memory: 116948 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+stack = []
+answer = []
+cur = 1
+flag = 0
+
+for i in range(n):
+  num = int(input())
+
+  while cur <= num:
+    stack.append(cur)
+    answer.append('+')
+    cur += 1
+
+  if stack[-1] == num:
+    stack.pop()
+    answer.append('-')
+  else:
+    print("NO")
+    flag = 1
+    break;
+
+if flag == 0:
+  for data in answer:
+    print(data)
