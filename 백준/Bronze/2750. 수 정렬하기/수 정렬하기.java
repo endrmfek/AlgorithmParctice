1703f85900fc179da5fe6d10b10314dfc24ffb75
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-11-12 16:02:24 +0900

    [Bronze II] Title: 수 정렬하기, Time: 180 ms, Memory: 14984 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,29 +1,24 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		for(int i=0; i<n-1; i++) {
-			for(int j=0; j<n-1-i; j++) {
-				if(arr[j] > arr[j+1]) {
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
-			}
-		}
-		
-		for(int i=0; i<n; i++) {
-			System.out.println(arr[i]);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i=0; i<n; i++) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(arr);
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
 }
