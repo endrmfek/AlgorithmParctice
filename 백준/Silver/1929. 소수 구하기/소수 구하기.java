53ee06527612ba8483f69dbfad5b72bbe16e354c
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-11-16 15:35:21 +0900

    [Silver III] Title: 소수 구하기, Time: 872 ms, Memory: 30588 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,32 +1,41 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt(); //m이상
-		int n = sc.nextInt(); //n이하 소수 찾기
-		
-		int[] number = new int[n+1];
-		
-		for(int i=2; i<n+1; i++) {
-			number[i] = i;
-		}
-		
-		for(int i=2; i<=Math.sqrt(n); i++) { // 최대 숫자의 제곱근까지만 탐색해도 됨.
-			
-			if(number[i] == 0) continue;
-			
-			for(int j=i+i; j<=n; j=j+i) { //첫번째 i는 그냥 냅두기 위해 
-				//배수 지워
-				number[j] = 0;
-			}
-		}
-		
-		for(int i=m; i<n+1; i++) {
-			if(number[i] != 0) {
-				System.out.println(number[i]);
-			}
-		}
-	}
+
+    static int N;
+    static int M;
+    static boolean[] arr;
+    static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        arr = new boolean[N+1];
+        Arrays.fill(arr, true);
+
+        arr[0] = false;
+        arr[1] = false;
+
+        for (int i=2; i <= Math.sqrt(N)+1; i++) {
+            if (arr[i]) {
+                for (int j=i*i ; j<=N; j=j+i) {
+                    arr[j] = false;
+                }
+            }
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (arr[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
