34b4ed8c024ba8e0679486c799ab591f79dad994
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2023-12-26 22:27:59 +0900

    [Silver I] Title: 정수 삼각형, Time: 280 ms, Memory: 29408 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,39 +1,43 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[][] arr = new int[n][n];
-		int[][] dp = new int[n][n];
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<=i; j++) {
-				arr[i][j] = sc.nextInt();
-				dp[i][j] = arr[i][j];
-			}
-		}
-		
-		//대각선 왼쪽 또는 대각선 오른쪽
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<=i; j++) {
-				int leftup, rightup;
-				if(j==0) leftup = 0;
-				else leftup = dp[i-1][j-1];
-				
-				if(j==i) rightup = 0;
-				else rightup = dp[i-1][j];
-				
-				dp[i][j] = dp[i][j] + Math.max(rightup, leftup);
-				
-			}
-		}
-		
-		int result =0;
-		for(int i=0; i<n; i++) {
-			result = Math.max(result , dp[n-1][i]);
-		}
-		System.out.println(result);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] data = new int[n][n];
+        int[][] dp = new int[n][n];
+
+        for(int i=0; i<n; i++) {
+            String[] row = br.readLine().split(" ");
+            for(int j=0; j<row.length; j++) {
+                data[i][j] = Integer.parseInt(row[j]);
+            }
+        }
+
+        dp[0][0] = data[0][0];
+
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<=i; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i-1][j] + data[i][j];
+                    continue;
+                }
+
+                if(j == i) {
+                    dp[i][j] = dp[i-1][j-1] + data[i][j];
+                    continue;
+                }
+
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + data[i][j];
+            }
+        }
+
+        System.out.println(Arrays.stream(dp[n - 1]).max().getAsInt());
+
+    }
+
 }
