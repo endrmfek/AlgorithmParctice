bdb324ee2843788e92f57a9c2821c6aa0208949b
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2025-03-13 21:36:27 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,24 +1,28 @@
-n ,m = map(int,input().split())
+def calc_min(ny, nx):
+    global chase1, chase2, board
+    cnt1 = cnt2 = 0
 
-data = []
-count = []
+    for i in range(8):
+        for j in range(8):
+            cnt1 += (board[ny+i][nx+j] != chase1[i][j])
+            cnt2 += (board[ny+i][nx+j] != chase2[i][j])
 
-for _ in range(n):
-  data.append(input())
+    return min(cnt1, cnt2)
 
-for i in range(n-7):
-  for j in range(m-7):
-    one = 0
-    two = 0
+chase1 = [['' for _ in range(8)] for _ in range(8)]
+chase2 = [['' for _ in range(8)] for _ in range(8)]
 
-    for a in range(i , i+8):
-      for b in range(j , j+8):
-        if (a+b) % 2 == 0:
-          if data[a][b] != 'W': one +=1
-          if data[a][b] != 'B': two +=1
-        else:
-          if data[a][b] != 'B': one += 1
-          if data[a][b] != 'W': two += 1
-    count.append(min(one,two))
+for i in range(8):
+    for j in range(8):
+        chase1[i][j] = 'W' if (i+j) % 2 == 0 else 'B'
+        chase2[i][j] = 'B' if (i+j) % 2 == 0 else 'W'
 
-print(min(count))
+N,M = map(int, input().split())
+board = [input() for _ in range(N)]
+
+result = int(1e9)
+for y in range(N-7):
+    for x in range(M-7):
+        result = min(result, calc_min(y,x))
+
+print(result)
