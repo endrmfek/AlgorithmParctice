d0b21d2b2519b5dbf90e1c135508394449f8e3ac
MINSOO KANG <97136279+endrmfek@users.noreply.github.com>
2025-05-01 20:38:33 +0900

    Title: 함수를 이용한 윤년 판별, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250501/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,20 @@
+y = int(input())
+
+# Please write your code here.
+def check(n):
+    if y % 4 != 0:
+        return False
+    
+    if y % 100 != 0:
+        return True
+    
+    if y % 400 == 0:
+        return True
+
+    return False
+    
+
+if check(y):
+    print("true")
+else:
+    print("false")
